5eeeb05700c887e85f3751ef412791ff48f81554
Dev_wanjohi <92452113+wanjohimiriam@users.noreply.github.com>
2024-04-06 11:31:15 +0300

    002

--- app/src/main/java/com/example/eventregistration/CaptureFragment.java
+++ app/src/main/java/com/example/eventregistration/CaptureFragment.java
@@ -2,7 +2,9 @@ package com.example.eventregistration;
 
 import static android.content.ContentValues.TAG;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +16,9 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
-//import com.example.eventregistration.OnSpinerItemClick;
-//import com.example.eventregistration.SpinnerDialog;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -32,21 +33,21 @@ import java.util.Timer;
 
 public class CaptureFragment extends Fragment {
     View view;
-    private EditText nameT,emailT,companyT,pnumberT,designationT;
+    private EditText nameT, emailT, companyT, pnumberT, designationT;
 
     private Button buttonSubmit;
     private Button addSubmit;
     private Button confirmSubmit;
     private ImageView search_img;
-    ArrayList<String> items=new ArrayList<>();
-//    private SpinnerDialog spinnerDialog;
+    ArrayList<String> items = new ArrayList<>();
+    //    private SpinnerDialog spinnerDialog;
     //private SpinnerDialog spinnerDialog;
     private JSONArray item_visitors;
-    private ArrayList<String>full_names;
-    private ArrayList<String>company_name;
-    private ArrayList<String>email;
-    private ArrayList<String>pnumber;
-    private ArrayList<String>designation;
+    private ArrayList<String> full_names;
+    private ArrayList<String> company_name;
+    private ArrayList<String> email;
+    private ArrayList<String> pnumber;
+    private ArrayList<String> designation;
     private Timer timer;
 
     private ListView listview;
@@ -56,14 +57,11 @@ public class CaptureFragment extends Fragment {
     private List<String> allItems;
     private List<String> displayedItems;
     DatabaseReference db, nameref, Cref, PNref, DesRef, Eref;
-    SendSmsManager smsManager;
-
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         view = inflater.inflate(R.layout.capturefragment, container, false);
-        
-
 
         nameT = view.findViewById(R.id.nameT);
         emailT = view.findViewById(R.id.emailT);
@@ -77,71 +75,82 @@ public class CaptureFragment extends Fragment {
         confirmSubmit.setOnClickListener(v -> post());
 
         Log.d("CaptureFragment", "View initialized successfully");
-        db= FirebaseDatabase.getInstance().getReference();
-        nameref=db.child("Name");
-        Cref=db.child("Company");
-        Eref=db.child("Email");
-        PNref=db.child("PhoneNumber");
-        DesRef=db.child("Designation");
+        db = FirebaseDatabase.getInstance().getReference();
+        nameref = db.child("Name");
+        Cref = db.child("Company");
+        Eref = db.child("Email");
+        PNref = db.child("PhoneNumber");
+        DesRef = db.child("Designation");
 
         return view;
     }
 
-    //
-    private void post()
-    {
-        // Log.d("debugging", "post: ");
-        if(nameT.getText().toString().equals(""))
-        {
+    private void post() {
+        if (nameT.getText().toString().isEmpty()) {
             nameT.setError("This field is required");
             nameT.requestFocus();
-        }
-        else if(companyT.getText().toString().equals(""))
-        {
+        } else if (companyT.getText().toString().isEmpty()) {
             companyT.setError("This field is required");
             companyT.requestFocus();
-        }
-        else if(emailT.getText().toString().equals(""))
-        {
+        } else if (emailT.getText().toString().isEmpty()) {
             emailT.setError("This field is required");
             emailT.requestFocus();
-        }
-        else if(pnumberT.getText().toString().equals(""))
-        {
+        } else if (pnumberT.getText().toString().isEmpty()) {
             pnumberT.setError("This field is required");
             pnumberT.requestFocus();
-        }
-        else if(designationT.getText().toString().equals(""))
-        {
+        } else if (designationT.getText().toString().isEmpty()) {
             designationT.setError("This field is required");
             designationT.requestFocus();
-        }
-        else
-        {
+        } else {
             final String name = nameT.getText().toString().trim().toUpperCase();
             final String company = companyT.getText().toString().trim().toUpperCase();
             final String email = emailT.getText().toString().trim();
             final String pnumber = pnumberT.getText().toString().trim();
             final String designation = designationT.getText().toString().trim();
 
-            sendToDB(name,company,email,pnumber,designation);
-            Toast.makeText(getContext(), "InsertedSuccessfully", Toast.LENGTH_SHORT).show();
-            Log.d(TAG, "post:"+name +company +email +pnumber+designation);
-
+            sendToDB(name, company, email, pnumber, designation);
+            sendSMSMessage();
         }
     }
 
     private void sendToDB(String name, String company, String email, String pnumber, String designation) {
-
         nameref.setValue(name);
         Cref.setValue(company);
         Eref.setValue(email);
         PNref.setValue(pnumber);
         DesRef.setValue(designation);
-
-
     }
 
+    protected void sendSMSMessage() {
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), android.Manifest.permission.SEND_SMS)) {
+                Toast.makeText(requireContext(), "SMS permission is required to send SMS.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(), new String[]{android.Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        } else {
+            // Permission already granted; send SMS
+            String message ;
+            message= "Hello Mr Karari Welcome to Karatina University. Hoping to see you again next time";
+            String phonenumber = "+254797917978";
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phonenumber, null, message, null, null);
+            Log.d(TAG, "sendSMSMessage: "+phonenumber);
+            Toast.makeText(getActivity(), "SMS sent.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    sendSMSMessage();
+                } else {
+                    Toast.makeText(getActivity(), "SMS permission denied.", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
 }
-
-
